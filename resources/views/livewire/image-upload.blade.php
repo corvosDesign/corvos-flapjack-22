5b9906a8daaac863334f6fcc17325f82569a3943
Adam Brewer <corvos.design2021@adams-macbook-pro.local>
2022-04-18 19:25:33 +0000

    image save

--- resources/views/livewire/image-upload.blade.php
+++ resources/views/livewire/image-upload.blade.php
@@ -10,7 +10,16 @@
 <div class=" pb-10">
 <form wire:submit.prevent="save">
 @csrf
-<div class="grid grid-col-2">
+<div class="grid grid-col-2 x-data="{ isUploading: false, progress: 0 }"
+
+x-on:livewire-upload-start="isUploading = true"
+
+x-on:livewire-upload-finish="isUploading = false"
+
+x-on:livewire-upload-error="isUploading = false"
+
+x-on:livewire-upload-progress="progress = $event.detail.progress"
+">
 <div class="row-span-full col-start-1 w-28 sm:w-40 overflow-hidden mr-3 mt-7 mb-7">
 
 @if ($photo)
@@ -23,19 +32,9 @@
 <div class="row-span-full col-start-2 grid place-content-evenly sm:pl-4">
 <div class="md:col-start-2">
 <!-- livewrie input test -->
-<div
 
-    x-data="{ isUploading: false, progress: 0 }"
 
-    x-on:livewire-upload-start="isUploading = true"
 
-    x-on:livewire-upload-finish="isUploading = false"
-
-    x-on:livewire-upload-error="isUploading = false"
-
-    x-on:livewire-upload-progress="progress = $event.detail.progress"
-
->
 
     <!-- File Input -->
 
@@ -71,7 +70,7 @@
 </div>
 
 <div class="md:col-start-4">
-<button class="font-light w-20 text-sm bg-blueline hover:border-2 shadow focus:bg-blue-500 active:bg-blue-500 rounded p-2 text-gray-50" type="submit">save </button>
+<button x-bind:disabled="isUploading" class="font-light w-20 text-sm bg-blueline hover:border-2 shadow focus:bg-blue-500 active:bg-blue-500 rounded p-2 text-gray-50 disable:text-site-white disabled:bg-site-gray" type="submit">save </button>
 </div>
 </div>
 </div>
