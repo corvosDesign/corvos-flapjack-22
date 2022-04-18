0799a97b5a7ad431c9979876b6045abf836184c2
Adam Brewer <corvos.design2021@adams-macbook-pro.local>
2022-04-18 20:22:38 +0000

    style change

--- resources/views/components/comment-list.blade.php
+++ resources/views/components/comment-list.blade.php
@@ -1,4 +1,4 @@
-<div class="grid gap-1 text-blueline">
+<div class="grid gap-1 text-blueline mt-4 mb-4 h-40 overflow-scroll">
 @foreach($comments as $comment)
 <p class="text-sm font-light col-span-full">{{$comment->comment}}</p>
 @endforeach
